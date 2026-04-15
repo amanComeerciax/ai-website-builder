cbba3a061c5ec9e07822248f71a7947fafaa620f
Mo_Amaan <mohammad.aman@commerciax.com>
2026-04-15 15:43:14 +0530

    deploy error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,10 +12,14 @@ const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ClerkProvider
     publishableKey={clerkPubKey}
+    // signInUrl="/login"
+    // signUpUrl="/signup"
+    // signInFallbackRedirectUrl="/dashboard"
+    // signUpFallbackRedirectUrl="/dashboard"
     signInUrl="/login"
     signUpUrl="/signup"
-    signInFallbackRedirectUrl="/dashboard"
-    signUpFallbackRedirectUrl="/dashboard"
+    afterSignInUrl="/dashboard"
+    afterSignUpUrl="/dashboard"
     appearance={{
       baseTheme: dark,
       variables: {
